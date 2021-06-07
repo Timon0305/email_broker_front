3cad3438f8d420c53387982be33dd2557c1cd7c4
Timon0305 <timon0305@outlook.com>
2021-06-08 07:33:48 +0800

    fixed router

--- src/app/app.routing.ts
+++ src/app/app.routing.ts
@@ -1,25 +1,13 @@
 import { Route } from '@angular/router';
-import { AuthGuard } from 'app/core/auth/guards/auth.guard';
-import { NoAuthGuard } from 'app/core/auth/guards/noAuth.guard';
 import { LayoutComponent } from 'app/layout/layout.component';
 import { InitialDataResolver } from 'app/app.resolvers';
 
-// @formatter:off
-/* eslint-disable max-len */
-/* eslint-disable @typescript-eslint/explicit-function-return-type */
 export const appRoutes: Route[] = [
 
-    // Redirect empty path to '/dashboards/project'
-    {path: '', pathMatch : 'full', redirectTo: 'apps/chat'},
+    {path: '', pathMatch : 'full', redirectTo: 'email'},
 
-    // Redirect signed in user to the '/dashboards/project'
-    //
-    // After the user signs in, the sign in page will redirect the user to the 'signed-in-redirect'
-    // path. Below is another redirection for that path to redirect the user to the desired
-    // location. This is a small convenience to keep all main routes together here on this file.
-    {path: 'signed-in-redirect', pathMatch : 'full', redirectTo: 'apps/chat'},
+    {path: 'signed-in-redirect', pathMatch : 'full', redirectTo: 'email'},
 
-    // Admin routes
     {
         path       : '',
         component  : LayoutComponent,
@@ -27,17 +15,13 @@ export const appRoutes: Route[] = [
             initialData: InitialDataResolver,
         },
         children   : [
-            // Pages
+
             {path: 'pages', children: [
                 {path: 'authentication', loadChildren: () => import('app/modules/admin/pages/authentication/authentication.module').then(m => m.AuthenticationModule)},
             ]},
 
-            {path: 'apps', children: [
-
-                {path: 'chat', children: [
+            {path: 'email', children: [
                     {path: '', loadChildren: () => import('app/modules/admin/ui/forms/layouts/layouts.module').then(m => m.FormsLayoutsModule)},
-                ]},
-
             ]},
 
             {path: '**', redirectTo: ''}
